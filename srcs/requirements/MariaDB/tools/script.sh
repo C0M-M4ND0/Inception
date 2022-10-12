0536bd94cda9e319703e9db891bfca6f32a2da5c
C0M-M4ND0 <omarabdelhadi1337@gmail.com>
2022-10-12 09:55:39 -0400

    done

--- srcs/requirements/MariaDB/tools/script.sh
+++ srcs/requirements/MariaDB/tools/script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-cat << EOF > file
+cat << EOF > file.sql
 create database if not exists $MYSQL_DATABASE;
 grant all privileges on $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 flush privileges;
 EOF
 
-mysql_install_db --basedir=/usr  --datadir=/var/lib/mysql > /dev/null
+mysql_install_db  &> /dev/null
 service mysql start
-mysql -u root  < file
-rm -rf file
+mysql -u root  < file.sql
